b033c58c13fb0d6eaf261274175c55709ba3980f
dev.marcoscardoso@gmail.com <dev.marcoscardoso@gmail.com>
2023-04-08 09:52:58 -0300

    rotas e express

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from 'express'; 
+import cors from 'cors'; 
+import routes from './routes'; 
+import path from 'path'; 
+import { errors } from 'celebrate'; 
+  
+const app = express(); 
+  
+app.use(cors(/*{ origin:'http://www' 
+ }*/)); 
+//para habilitar a utilização de json nas requisições com body em json 
+app.use(express.json()); 
+app.use(routes); 
+  
+app.use('/uploads', express.static(path.resolve(__dirname, '../', 'uploads'))); 
+  
+app.use(errors()); 
+  
+app.listen(3333);
